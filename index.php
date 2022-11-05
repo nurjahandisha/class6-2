65f3236f91eecdf35409d0270b02b281ca4b4ce7
nurjahandisha <116632760+nurjahandisha@users.noreply.github.com>
2022-11-05 14:41:56 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class ="col-5 mx-auto mt-5">
+        <form action="practice.php" method="post">
+            <label  class="w-100">
+                 Name
+                <input type="text" class="form-control" name="name">
+</label>
+<label  class="w-100">
+                Age
+                <input type="text" class="form-control" name="age">
+</label>
+<button class="btn btn-primary mt-2 w-100">Submit</button>
+</form>
+</div>
+</body>
+</html>
